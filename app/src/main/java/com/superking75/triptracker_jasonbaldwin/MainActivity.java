cfe55e1faf329ba275714331696f1a36237ce595
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-04-23 09:48:31 -0400

    First

---
+++ app/src/main/java/com/superking75/triptracker_jasonbaldwin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.superking75.triptracker_jasonbaldwin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
